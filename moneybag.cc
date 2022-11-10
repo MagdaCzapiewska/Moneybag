fd0a92cff7e89837f43aab75841cbbb07778f95f
Magda <magdalena.czapiewska001@wp.pl>
2022-11-10 18:18:33 +0100

    Thursday work

--- moneybag.cc
+++ moneybag.cc
@@ -1,1 +1,52 @@
 #include "moneybag.h"
+#include <iostream>
+
+// Metody klasy Moneybag.
+
+Moneybag::Moneybag(coin_number_t ll, coin_number_t ss, coin_number_t dd) : l(ll), s(ss), d(dd) {}
+
+Moneybag::Moneybag(const Moneybag& moneybag) {
+    std::cout << moneybag.livre_number() << " " << moneybag.solidus_number() << " " << moneybag.denier_number() << "\n";
+    Moneybag(moneybag.livre_number(), moneybag.solidus_number(), moneybag.denier_number());
+}
+
+Moneybag::coin_number_t Moneybag::livre_number() const {
+    return this->l;
+}
+
+Moneybag::coin_number_t Moneybag::solidus_number() const {
+    return this->s;
+}
+
+Moneybag::coin_number_t Moneybag::denier_number() const {
+    return this->d;
+}
+
+std::ostream& operator<< (std::ostream &strm, const Moneybag& rhs) {
+
+    strm << "(" << rhs.livre_number() << (rhs.livre_number() == 1 ? " livre, " : " livres, ") <<
+    rhs.solidus_number() << (rhs.solidus_number() == 1 ? " solidus, " : " soliduses, ") <<
+    rhs.denier_number() << (rhs.denier_number() == 1 ? " denier" : " deniers") << ")\n";
+
+    return strm;
+}
+
+Moneybag::operator bool() const {
+    if ((this->l == 0) && (this->s == 0) && (this->d == 0)) {
+        return false;
+    }
+    return true;
+}
+
+int main() {
+    Moneybag m1 = Moneybag(1, 10, 19);
+    std::cout << m1;
+    //m1.operator<<(std::cout);
+    Moneybag m3(0, 0, 0);
+    std::cout << m3;
+    Moneybag m4(m1);
+    std::cout << m1;
+    std::cout << m4;
+
+    return 0;
+}
--- moneybag.h
+++ moneybag.h
@@ -1,4 +1,91 @@
 #ifndef __MONEYBAG_H__
 #define __MONEYBAG_H__
 
+#include <initializer_list>
+#include <cstdint>
+#include <sstream>
+
+constexpr uint64_t denier_denier = 1;
+constexpr uint64_t solid_denier = 12;
+constexpr uint64_t livre_denier = 240;
+
+class Denier {
+public:
+    Denier();
+};
+
+class Livre {
+
+public:
+    Livre();
+};
+
+class Solidus {
+
+public:
+    Solidus();
+};
+
+class Moneybag {
+
+private:
+    //constinit static Livre l;
+    //constinit static Solidus s;
+    //constinit static Denier d;
+
+    uint64_t l;
+    uint64_t s;
+    uint64_t d;
+
+public:
+    using coin_number_t = uint64_t;
+
+    //tworzenie sakiewki na podstawie liczby poszczególnych monet, np.
+    //Moneybag(1, 2, 3) powinno stworzyć sakiewkę, w której znajdują się 1 liwr,
+    //2 solidy i 3 denary; nie powinno być natomiast możliwości utworzenia
+    //obiektu bez podania liczby monet;
+
+    Moneybag(coin_number_t dd, coin_number_t ss, coin_number_t ll);
+
+    Moneybag(const Moneybag& moneybag);
+
+    //tworzenie sakiewki przez skopiowanie innej sakiewki;
+
+    // Odczytanie liczby poszczególnych monet za pomocą odpowiednio metod
+    //livre_number(), solidus_number() i denier_number();
+    coin_number_t livre_number() const;
+    coin_number_t solidus_number() const;
+    coin_number_t denier_number() const;
+
+
+    // Wykonywanie operacji dodawania, odejmowania zawartości sakiewek oraz
+    //mnożenia zawartości sakiewki przez liczbę całkowitą nieujemną;
+
+    // Wysłanie na strumień wyjściowy napisu reprezentującego zawartość sakiewki;
+    //std::ostream& operator<<(std::ostream &strm);
+
+    //porównywanie zawartości sakiewek – na sakiewkach zdefiniowany jest
+    //naturalny porządek częściowy;
+
+    // Rzutowanie sakiewki na typ bool, informujące, czy sakiewka jest niepusta
+    //(zawiera choć jedną monetę).
+    operator bool() const;
+};
+
+class Value {
+
+    //utworzenie wartości na podstawie sakiewki lub liczby całkowitej
+    //nieujemnej;
+
+    //kopiowanie wartości;
+
+    //porównywanie wartości – na wartościach zdefiniowany jest naturalny
+    //porządek liniowy;
+
+    //porównywanie wartości z liczbami całkowitymi nieujemnymi;
+
+    //rzutowanie wartości na obiekt klasy string reprezentujący wartość
+    //w zapisie dziesiętnym.
+};
+
 #endif // __MONEYBAG_H__
